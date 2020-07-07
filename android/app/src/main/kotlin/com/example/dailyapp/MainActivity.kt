0b82a89bf41aef30e1a4220c2529b53f83d1e404
kureharyosuke <ohhayo.com@gmail.com>
2020-07-07 20:02:35 +0900

    #2.0 Search

---
+++ android/app/src/main/kotlin/com/example/dailyapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dailyapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:dailyapp/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: "Cairo",
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context)
+        .size; //this gonna give us total height and with of our device
+    return Scaffold(
+        body: Stack(
+      children: <Widget>[
+        Container(
+          // Here the height of the container is 45% of our total height
+          height: size.height * .45,
+          decoration: BoxDecoration(
+            color: Color(0xFFF5CEB8),
+            image: DecorationImage(
+              alignment: Alignment.centerLeft,
+              image: AssetImage("assets/images/undraw_pilates_gpdb.png"),
+            ),
+          ),
+        ),
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                      alignment: Alignment.center,
+                      height: 52,
+                      width: 52,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF2BEA1),
+                        shape: BoxShape.circle,
+                      ),
+                      child: SvgPicture.asset("assets/icons/menu.svg")),
+                ),
+                Text(
+                  "Good Mornig \nToday",
+                  style: Theme.of(context)
+                      .textTheme
+                      .display1
+                      .copyWith(fontWeight: FontWeight.w900),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 30),
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(29.5)),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      icon: SvgPicture.asset("assets/icons/search.svg"),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    childAspectRatio: .85,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                    children: <Widget>[
+                      CategoryCard(
+                        title: "Diet Recommendation",
+                        svgSrc: "assets/icons/Hamburger.svg",
+                      ),
+                      CategoryCard(
+                        title: "Meditation",
+                        svgSrc: "assets/icons/Meditation_women_small.svg",
+                      ),
+                      CategoryCard(
+                        title: "Kegel Exercises",
+                        svgSrc: "assets/icons/Excrecises.svg",
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    ));
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  final String svgSrc;
+  final String title;
+  const CategoryCard({
+    Key key,
+    this.svgSrc,
+    this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(13),
+      ),
+      child: Column(
+        children: <Widget>[
+          Spacer(),
+          SvgPicture.asset(svgSrc),
+          Spacer(),
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.title.copyWith(fontSize: 15),
+          )
+        ],
+      ),
+    );
+  }
+}
